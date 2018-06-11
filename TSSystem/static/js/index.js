6a43862cb1a0970b9eddc33e1ef6186b28ebc056
Jacen_Chou <a774845313@163.com>
2018-06-11 14:46:38 +0800

    增加向后端传送身份，删除测试用swal

--- TSSystem/static/js/index.js
+++ TSSystem/static/js/index.js
@@ -20,7 +20,7 @@ function login() {
     }
 
     if (document.getElementById("radio1").checked == false && document.getElementById("radio2").checked == false && document.getElementById("radio3").checked == false) {
-        swal("请选择身份","");
+        swal("请选择身份", "", "error");
         flag = 'n';
     }
     if (flag == 'n') return false;
@@ -40,7 +40,7 @@ function login() {
                 if(data.msg=="2") window.location.href="/teacher";
                 if(data.msg=="3") window.location.href="/student";
             } else if (data.status == 'fail') {
-                swal(data.msg,"","error");
+                swal(data.msg, "", "error");
             }
         }
     });
@@ -52,28 +52,28 @@ function forgetPassword() {
 
     if (form1.username.value == "" && document.getElementById("radio1").checked == false && document.getElementById("radio2").checked == false && document.getElementById("radio3").checked == false) {
         form1.username.style.borderColor = 'red';
-        swal("请填写您的用户名和选择身份!", "");
+        swal("请填写您的用户名和选择身份!", "", "error");
         form1.username.focus();
         return false;
     }
 
     if (form1.username.value == "") {
         form1.username.style.borderColor = 'red';
-        swal("请填写您的用户名!","");
+        swal("请填写您的用户名!", "", "error");
         form1.username.focus();
         flag = 'n';
     }
 
     if (document.getElementById("radio1").checked == false && document.getElementById("radio2").checked == false && document.getElementById("radio3").checked == false) {
-        swal("请选择身份","");
+        swal("请选择身份", "", "error");
         flag = 'n';
     }
     if (flag == 'n') return false;
-    swal("已向您的邮箱发送初始密码，请查看并及时修改！","","success");
 
     $.ajax({
         data: {
-            'username': document.getElementById("username").value
+            'username': document.getElementById("username").value,
+            'identify': $('input:radio:checked').val() //身份 管理员：1 教师：2 学生：3
         },
         type: "post", //提交方式
         dataType: "json", //数据类型
